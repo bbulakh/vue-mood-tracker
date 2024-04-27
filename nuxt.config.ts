2edfb6bde625d932f8dcadd36bacd3bc384e35c1
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-27 22:57:06 +0300

    add inter font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false, 
-  srcDir: 'src'
-})
+  ssr: false,
+  srcDir: "src",
+});
