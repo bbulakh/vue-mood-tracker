95a9d9b62dca1e87cca7afb8232ef852bc8963d7
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-28 23:16:34 +0300

    add draft UserGreeting component

---
+++ src/components/UserGreeting.vue
@@ -0,0 +1,12 @@
+<script setup>
+  let userName = ref("Bogdan");
+  import "./assets/styles/custom.css"; 
+</script>
+
+<template>
+  <h1 class="text-2xl font-bold">Hello, {{ userName }}!</h1>
+  <p>How is your day?</p>
+</template>
+
+<style>
+</style>
