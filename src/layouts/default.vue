b0008e067f3296a31b5931bfcd7524e1a5a79265
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-29 02:28:14 +0300

    add draft mobile menu

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,36 @@
 <template>
-    <div class="px-5 py-3 h-screen"><NuxtPage></NuxtPage></div>
+    <div class="px-5 py-5 h-screen"><NuxtPage></NuxtPage></div>
+    <v-layout class="overflow-visible" style="height: 46px;">
+    <v-bottom-navigation
+      v-model="value"
+      :bg-color="red"
+      mode="shift"
+    >
+      <v-btn>
+        <v-icon>mdi-television-play</v-icon>
+
+        <span>Video</span>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-music-note</v-icon>
+
+        <span>Music</span>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-book</v-icon>
+
+        <span>Book</span>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-image</v-icon>
+
+        <span>Image</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
 </template>
 
 <script>
