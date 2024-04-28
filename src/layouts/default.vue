ddb9a8430410d991890a00d068380da533f63cfd
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-28 23:13:32 +0300

    add default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <NuxtPage></NuxtPage>
+    <div class="px-5 py-3 h-screen"><NuxtPage></NuxtPage></div>
 </template>
 
 <script>
-import "./assets/styles/custom.css"; 
+    import "./assets/styles/custom.css"; 
 </script>
