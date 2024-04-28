9fa151291a5b02e3876d1a0be8d3613d7ed2007f
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-29 02:27:22 +0300

    fix grid layout

---
+++ src/components/MoodStats.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="grid grid-rows-2 grid-flow-col gap-2 py-2 h-40 max-w-[300px]">
+        <div class="flex justify-center row-span-2 items-center rounded-2xl border"><NuxtLink to="/mood-entry">GOOD</NuxtLink></div>
+        <div class="flex justify-center row-span-1 items-center rounded-2xl border">20% sad</div>
+        <div class="flex justify-center row-span-1 items-center rounded-2xl border">50% average</div>
+    </div>
+</template>
